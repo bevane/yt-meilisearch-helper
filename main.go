a4b3e26c6c191068b0332a3c50c5d9aaab396fe3
bevane <bevane@live.com>
2025-04-05 14:55:12 +0700

    feat: get channel url from .env

--- main.go
+++ main.go
@@ -13,7 +13,9 @@ type VideoProcessingStatus map[string]string
 func main() {
 	godotenv.Load(".env")
 	dataPath := os.Getenv("DATA_PATH")
+	channelUrl := os.Getenv("CHANNEL_URL")
 	fmt.Printf("Setting project directory to %s\n", dataPath)
+	fmt.Printf("Downloading and Processing videos for %s\n", channelUrl)
 	err := initDataDir(dataPath)
 	if err != nil {
 		slog.Error(fmt.Sprintf("Error initializing project folder: %v\n", err.Error()))
